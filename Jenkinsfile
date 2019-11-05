048df0ac9526c234b983eb3f28eb4f3af47b0f35
Eduardo Raupp Pereira <eduardo.raupp_pereira@roche.com>
2019-11-05 16:18:26 +0100

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,56 @@
+#!groovy
+pipeline {
+
+  agent any
+
+  environment {
+      BRANCH_NAME=env.GIT_BRANCH.replace("origin/", "")
+  }
+
+  tools {
+	maven 'maven'
+  }
+
+  stages {
+    stage('Build') {
+      steps {
+	   sh "mvn clean verify"
+      }
+    }
+    stage('Publish Pacts') {
+      steps {
+        //-- set prod if want to deploy prod tag
+        sh 'mvn pact:publish -Dpactbroker.url=${PACT_BROKER_URL} -Dpact.consumer.version=${GIT_COMMIT} -Dpact.tag=prod'
+      }
+    }
+    stage('Check Pact Verifications') {
+      steps {
+        sh 'curl -LO https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.61.1/pact-1.61.1-linux-x86_64.tar.gz'
+        sh 'tar xzf pact-1.61.1-linux-x86_64.tar.gz'
+        dir('pact/bin') {
+          sh "./pact-broker can-i-deploy --retry-while-unknown=12 --retry-interval=10 -a person-consume -b http://pact_broker -e ${GIT_COMMIT}"
+        }
+      }
+    }
+    stage('Deploy') {
+      when {
+        branch 'master'
+      }
+      steps {
+        echo 'Deploying to prod now...'
+      }
+    }
+    stage('Tag Pact') {
+      steps {
+          // -t prod -- after the version
+        dir('pact/bin') {
+          sh "./pact-broker create-version-tag -a person-consume -b http://pact_broker -e ${GIT_COMMIT}"
+         // sh "./pact-broker create-version-tag -a messaging-app2 -b http://pact_broker -e ${GIT_COMMIT}"
+       //   sh "./pact-broker create-version-tag -a messaging-app3 -b http://pact_broker -e ${GIT_COMMIT}"
+        //  sh "./pact-broker create-version-tag -a userclient -b http://pact_broker -e ${GIT_COMMIT}"
+        }
+      }
+    }
+  }
+
+}
---
+++ src/test/java/cdc/PersonContractTest.java
@@ -0,0 +1,151 @@
+package cdc;
+
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
+import cdc.rule.RandomPortRule;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.pactfoundation.consumer.dsl.LambdaDsl;
+import okhttp3.*;
+import okio.BufferedSink;
+import org.apache.http.HttpStatus;
+import org.junit.*;
+
+import java.io.IOException;
+
+
+public class PersonContractTest {
+    @ClassRule
+    public static RandomPortRule randomPort = new RandomPortRule();
+
+    @Rule
+    public PactProviderRuleMk2 mockedProviderServer = new PactProviderRuleMk2("person-provider", "127.0.0.1",
+            randomPort.getPort(), this);
+
+    @Pact(consumer = "person-consumer", provider = "person-provider")
+    public RequestResponsePact savePerson(PactDslWithProvider builder) {
+
+        return builder.given(
+                "A person is saved")
+                .uponReceiving("POST REQUEST")
+                .path("/person")
+                .method("POST")
+                .body("{\"name\":\"Roche\"}")
+                .willRespondWith()
+                .status(201)
+                .toPact();
+    }
+
+    @PactVerification(fragment = "savePerson")
+    @Test
+    public void generateSavePersonContract() throws IOException {
+        //Arrange
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        String url = mockedProviderServer.getUrl().concat("/person");
+        Request request = new Request.Builder().url(url).post(RequestBody.create(JSON, "{\"name\":\"Roche\"}")).build();
+        int expectedStatusCode = HttpStatus.SC_CREATED;
+
+        //Act
+        int actualStatus = new OkHttpClient().newCall(request).execute().code();
+
+        //Assert
+        Assert.assertEquals(expectedStatusCode, actualStatus);
+    }
+
+    @Pact(consumer = "person-consumer", provider = "person-provider")
+    public RequestResponsePact updatePerson(PactDslWithProvider builder) {
+
+        return builder.given(
+                "A person is saved")
+                .uponReceiving("PUT REQUEST")
+                .path("/person/1")
+                .method("PUT")
+                .body("{\"name\":\"updated Roche\",\"id\":1}")
+                .willRespondWith()
+                .status(200)
+                .toPact();
+    }
+
+    @PactVerification(fragment = "updatePerson")
+    @Test
+    public void generateUpdatePersonContract() throws IOException {
+        //Arrange
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        String url = mockedProviderServer.getUrl().concat("/person/1");
+        Request request = new Request.Builder().url(url).put(RequestBody.create(JSON, "{\"name\":\"updated Roche\",\"id\":1}")).build();
+        int expectedStatusCode = HttpStatus.SC_OK;
+
+        //Act
+        int actualStatus = new OkHttpClient().newCall(request).execute().code();
+
+        //Assert
+        Assert.assertEquals(expectedStatusCode, actualStatus);
+    }
+
+    @Pact(consumer = "person-consumer", provider = "person-provider")
+    public RequestResponsePact getPersonById(PactDslWithProvider builder) {
+
+        return builder.given(
+                "A person is requested with an existing id")
+                .uponReceiving("A GET REQUEST")
+                .path("/person/1")
+                .method("GET")
+                .willRespondWith()
+                .status(200)
+                .body(LambdaDsl.newJsonBody(body -> {
+                    body.id("id", 1l).stringValue("name", "Roche");
+                }).build())
+                .toPact();
+    }
+
+    @PactVerification(fragment = "getPersonById")
+    @Test
+    public void generateGetPersonContract() throws IOException {
+        //Arrange
+        Long expectedId = 1l;
+        String expectedName = "Roche";
+        String url = mockedProviderServer.getUrl().concat("/person/").concat(expectedId.toString());
+        Request request = new Request.Builder().url(url).build();
+        ObjectMapper mapper = new ObjectMapper();
+
+        //Act
+        String actualJsonData = new OkHttpClient().newCall(request).execute().body().string();
+
+        //Assert
+        JsonNode jsonNode = mapper.readTree(actualJsonData);
+        Assert.assertEquals(expectedId.toString(), jsonNode.path("id").toString());
+        Assert.assertEquals(expectedName, jsonNode.get("name").toString().replaceAll("\"", ""));
+    }
+
+    @Pact(consumer = "person-consumer", provider = "person-provider")
+    public RequestResponsePact deletePerson(PactDslWithProvider builder) {
+
+        return builder.given(
+                "A person is requested with an existing id")
+                .uponReceiving("A GET REQUEST")
+                .path("/person/1")
+                .method("DELETE")
+                .willRespondWith()
+                .status(200)
+                .toPact();
+    }
+
+    @PactVerification(fragment = "deletePerson")
+    @Test
+    public void generateDeletePersonContract() throws IOException {
+        //Arrange
+        Long id = 1l;
+        String url = mockedProviderServer.getUrl().concat("/person/").concat(id.toString());
+        Request request = new Request.Builder().url(url).delete().build();
+        int expectedStatusCode = HttpStatus.SC_OK;
+
+        //Act
+        int actualStatus = new OkHttpClient().newCall(request).execute().code();
+
+        //Assert
+        Assert.assertEquals(expectedStatusCode, actualStatus);
+    }
+}
