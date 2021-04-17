b1061b650af29b9e896b89cce27f94a176d1ed24
Eduardo Raupp Pereira <eduardo.raupp_pereira@roche.com>
2021-04-17 18:47:48 +0200

    simulate commit

--- src/test/java/cdc/PersonContractTest.java
+++ src/test/java/cdc/PersonContractTest.java
@@ -123,7 +123,7 @@ public class PersonContractTest {
         //Assert
         JsonNode jsonNode = mapper.readTree(actualJsonData);
         Assert.assertEquals(expectedId.toString(), jsonNode.path("id").toString());
-        Assert.assertEquals(expectedName, jsonNode.get("name"));
+        Assert.assertEquals(expectedName, jsonNode.get("name").asInt());
     }
 
     @Pact(consumer = "person-consumer", provider = "person-provider")
